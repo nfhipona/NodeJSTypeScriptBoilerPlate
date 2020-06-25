34bf6767e32bb5bfb6ca4c2e548d03d8c68b03f8
Neil Francis Ramirez Hipona <nferocious76@gmail.com>
2020-06-25 02:43:20 -0700

    Add constants

---
+++ src/config/constant.ts
@@ -0,0 +1,20 @@
+'use strict';
+
+/** PERMISSION **/
+const PERMISSION_DENIED: string     = 'Permission denied';
+const FORBIDDEN_REQUEST: string     = 'Forbidden request';
+const SERVICE_UNAVAILABLE: string   = 'Service unavailable';
+const SERVER_ERROR: string          = 'Server error';
+const SERVER_MAINTENANCE: string    = 'Maintenance: Server is undergoing service upgrade. Please return later';
+const UNAUTHORIZED_REQUEST: string  = 'Unauthorized request';
+const DATABASE_CONN_ERROR: string   = 'Connection error';
+
+export default {
+    PERMISSION_DENIED,
+    FORBIDDEN_REQUEST,
+    SERVICE_UNAVAILABLE,
+    SERVER_ERROR,
+    SERVER_MAINTENANCE,
+    UNAUTHORIZED_REQUEST,
+    DATABASE_CONN_ERROR
+};
